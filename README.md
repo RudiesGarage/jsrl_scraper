b12117f861a593d0df7f60e96a364291c2e4fa2d
Mcdonoughd <dannmcdonough@gmail.com>
2021-09-06 16:50:23 -0400

    added regex to make faster, print station list

--- README.md
+++ README.md
@@ -1,28 +1,30 @@
 # jsrl_scraper
 Web scraper for jetsetradio.live, as [@SleepyLark](https://github.com/sleepylark) will be away for some time
 
-usage: 
-
-    python jsrl_scraper.py [-h] [-r [STATION|File of stations]] [-t] [-w ] [-wp]
+usage: jsrl_scraper.py [-h] [-v [VERBOSE [VERBOSE ...]]] [-stationList [STATIONLIST [STATIONLIST ...]]]
+                       [-musicList [MUSICLIST [MUSICLIST ...]]] [-radio [RADIO [RADIO ...]]]
+                       [-tv [TV [TV ...]]] [-graffiti [GRAFFITI [GRAFFITI ...]]]
+                       [-wallpaper [WALLPAPER [WALLPAPER ...]]] [-all [ALL [ALL ...]]]
 
 optional arguments:
-  
-  
-  - -h, --help            show this help message and exit
-  
-  
-  - -r [STATION|File of stations]]
-                        Download all music and images from a radio station; pass a file with station names to download
-                        more than one station
-  
-  - -t  Download all TV videos; (Warning this takes some time and about 70GB)
-  
-  
-  - -w   Download all default images from the wall;
-  
-  
-  - -wp  Download all gif wallpaper;
+  -h, --help            show this help message and exit
+  -v [VERBOSE [VERBOSE ...]]
+                        Print verbose while running
+  -stationList [STATIONLIST [STATIONLIST ...]]
+                        Print a list of stations
+  -musicList [MUSICLIST [MUSICLIST ...]]
+                        Print a list of songs; Given a station name
+  -radio [RADIO [RADIO ...]]
+                        Download all music & icons from a radio station; pass a file with station names
+                        to download more than one station
+  -tv [TV [TV ...]]     Download all TV videos; (Warning this takes some time and about 70GB)
+  -graffiti [GRAFFITI [GRAFFITI ...]]
+                        Download all default graffiti from the wall;
+  -wallpaper [WALLPAPER [WALLPAPER ...]]
+                        Download wallpaper;
+  -all [ALL [ALL ...]]  Download Everything (make sure you have enough space ~100GB);
+
 
 Example to download everything:
 
-    python jsrl_scraper.py -r stations.txt -t -w -wp
+    python jsrl_scraper.py -all
--- jsrl_scraper.py
+++ jsrl_scraper.py
@@ -1,6 +1,8 @@
-# JetSetRadio.live Scrapper
+#!/bin/python
+# JetSetRadio.live Scraper
 # 3/24/2021 by Daniel McDonough
-# For Preservation uses only,
+# 9/05/2021 - added regex quick look up
+
 
 import xml.etree.ElementTree as xml
 import shutil
@@ -10,14 +12,68 @@ import urllib.parse
 import os
 import argparse
 import requests  # to get image from the web
+import re
 
-jsrl_src = "https://jetsetradio.live/radio/stations/"
+isVerbose = [False]
+jsrl_url = "https://jetsetradio.live/"
+jsrl_src = jsrl_url+"radio/stations/"
 list_src = "/~list.js"
-tv_src = "https://jetsetradio.live/tv/APP/preloader/retrieveTotalFilesAndFilesList.php"
-wall_src = "https://jetsetradio.live/wall/APP/editor/library/"
-wallpaper_src = "https://jetsetradio.live/wallpaper/images/wallpaper"
+tv_src = jsrl_url+"tv/APP/preloader/retrieveTotalFilesAndFilesList.php"
+wall_src = jsrl_url+"wall/APP/editor/library/"
+wallpaper_src = jsrl_url+"wallpaper/images/wallpaper"
 
-def downloadTV(xmlfile):
+
+# Get the current JSRL stations
+def getStations():
+    stationList = []
+    resp = requests.get(jsrl_url)
+    # check if request was successful
+    if resp.status_code == 200:
+        # get regex string
+        decoded = resp.content.decode("utf-8")
+        regex = re.compile('(.*)<script src="radio/stations/(.*)/~list.js"></script>(.*)')
+        reg = regex.findall(decoded)
+        if reg is not None:
+            # print stations
+            for station in reg:
+                stationList.append(station[1])
+        else:
+            print("ERROR: No stations found")
+            exit(1)
+    else:
+        print("ERROR: Jetsetradio.live is down!")
+        exit(1)
+    return stationList
+
+# Get each song in a given station
+def getMusicList(station):
+    musicList = []
+    url = jsrl_src+station+list_src
+    resp = requests.get(url)
+    if resp.status_code == 200:
+        # get regex string
+        decoded = resp.content.decode("utf-8")
+        regex = re.compile('(.*)this\[stationName\+\'_tracks\'].length] = \"(.*)\";')
+        reg = regex.findall(decoded)
+        if reg is not None:
+            # print stations
+            for song in reg:
+                musicList.append(song[1])
+        else:
+            print("ERROR: No songs found in " + str(station))
+            exit(1)
+    else:
+        print("ERROR: Jetsetradio.live is down!")
+        exit(1)
+    return musicList
+
+# download TV mp4s
+def downloadTV():
+    with urllib.request.urlopen(tv_src) as response:
+        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+            shutil.copyfileobj(response, tmp_file)
+
+    xmlfile = open(tmp_file.name)
     # create element tree object
     tree = xml.parse(xmlfile)
     folder = "./tv"
@@ -27,7 +83,6 @@ def downloadTV(xmlfile):
     # get root element
     root = tree.getroot()
     for idx,child in enumerate(root):
-        print(child.text)
         if(child.text is not None):
             x = child.text.split('"')
             movies = []
@@ -41,27 +96,33 @@ def downloadTV(xmlfile):
                 if not os.path.exists(folder):
                     os.makedirs(folder)
 
-
                 link = "https://jetsetradio.live/tv/APP/videoplayer/ch"+str(idx)+"/"
                 output_folder = folder+"/"+video+".mp4"
                 vid = urllib.parse.quote(video)
-                print(link + vid + ".mp4")
+                if isVerbose:
+                    print("Downloading: "+link + vid + ".mp4")
                 try:
                     urllib.request.urlretrieve(link + vid + ".mp4", filename=output_folder)
                 except:
                     print("Could not find "+video)
 
-def downloadWall(cap=241):
+# download wall images
+def downloadWall():
     folder = "./wall/"
     if not os.path.exists(folder):
         os.makedirs(folder)
-
-    for img in range(1, cap):
+    img = 1
+    while True:
         link = wall_src + str(img) + ".png"
-        output_folder = folder + str(img) + ".png"
-        downloadFile(link, output_folder)
+        output = str(img) + ".png"
+        res = downloadFile(link, folder, output)
+        if res == 0:
+            img += 1
+        else:
+            break
 
 
+# download a single file
 def downloadFile(link,output_folder,fn):
     # Open the url image, set stream to True, this will return the stream content.
     r = requests.get(link, stream=True)
@@ -72,87 +133,94 @@ def downloadFile(link,output_folder,fn):
         r.raw.decode_content = True
 
         # Open a local file with wb ( write binary ) permission.
-        with open(fn, 'wb') as f:
+        with open(output_folder+fn, 'wb') as f:
             shutil.copyfileobj(r.raw, f)
-
-        print('File successfully Downloaded: ', output_folder)
+        if isVerbose:
+            print('File successfully Downloaded: ', output_folder+fn)
+        return 0
     else:
         print('File Couldn\'t be retrieved: ' + link)
+    return 1
+
 
 
 def downloadRadio(station):
-    # make source folder
-    folder = "./"+station
+    # make source folders
+    folder = "./"+station+"/"
+    imgfolder = folder+"/images/"
     if not os.path.exists(folder):
         os.makedirs(folder)
+    if not os.path.exists(imgfolder):
+        os.makedirs(imgfolder)
 
     # Download wallpapers icons etc:
     icon = jsrl_src + station + "/images/icon.png"
-    downloadFile(icon, folder,'icon.png')
+    downloadFile(icon, imgfolder,'icon.png')
     desc = jsrl_src + station + "/images/description.png"
-    downloadFile(desc, folder,'description.png')
-    wallpaper = jsrl_src + station + "/images/wallpaper.jpeg"
-    downloadFile(wallpaper, folder,'wallpaper.jpeg')
+    downloadFile(desc, imgfolder,'description.png')
     wallpaper = jsrl_src + station + "/images/wallpaper.jpg"
-    downloadFile(wallpaper, folder,'wallpaper.jpg')
+    if 1 == downloadFile(wallpaper, imgfolder,'wallpaper.jpg'):
+        # sometimes stations have jpeg not jpg
+        wallpaper = jsrl_src + station + "/images/wallpaper.jpeg"
+        downloadFile(wallpaper, imgfolder,'wallpaper.jpeg')
+
+
     list =  jsrl_src + station + "/~list.js"
     downloadFile(list,folder,'~list.js')
 
-
-    # get music list script
-    js_url = jsrl_src + station + list_src
-    with urllib.request.urlopen(js_url) as response:
-        with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-            shutil.copyfileobj(response, tmp_file)
-    # Read each line of JS Script
-    lines = []
-    with open(tmp_file.name) as html:
-        for line in html:
-            lines.append(line.rstrip())
-
-    # Get to the point with the music tracks
-    try:
-        indx = lines.index("//TRACKS")
-    except:
-        indx = 0
+    musicList = getMusicList(station)
 
     # for each music track...
-    for line in range(indx+1,len(lines),1):
-        # check if it is a music track
-        linecheck = lines[line].split(" = ")
-        if len(linecheck) == 2:
+    for song in musicList:
+        # download song
+        output_folder = folder+"/"+song+".mp3"
 
-            # track name is set from right side of the equals sign
-            songname = linecheck[1]
-            # clean up song title from quotes and ;
-            cleansong = songname[:-2]
-            cleansong = cleansong[1:]
-
-            # download song
-            output_folder = folder+"/"+cleansong+".mp3"
-
-            #Only download if new song
-            if not os.path.isfile(output_folder):
-
-                print("Downloading: "+jsrl_src + station + "/" + urllib.parse.quote(cleansong) + ".mp3")
-                urllib.request.urlretrieve(jsrl_src+station+"/"+urllib.parse.quote(cleansong)+".mp3",filename=output_folder)
+        #Only download if new song
+        if not os.path.isfile(output_folder):
+            if isVerbose:
+                print("Downloading: "+jsrl_src + station + "/" + urllib.parse.quote(song) + ".mp3")
+            urllib.request.urlretrieve(jsrl_src+station+"/"+urllib.parse.quote(song)+".mp3",filename=output_folder)
 
 
 def DownloadWallpaperImages():
+    if not os.path.exists("./wallpaper"):
+        os.makedirs("./wallpaper")
     for img in range(1, 13):
         link = wallpaper_src + str(img) + ".gif"
-        output_folder = "./wallpaper/wallpaper" + str(img) + ".gif"
-        downloadFile(link, output_folder)
+        output_folder = "./wallpaper/"
+        downloadFile(link, output_folder,"wallpaper"+str(img) + ".gif")
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('-r', help="Download all music and images from a radio station; pass a file with station names to download more than one station ", action='store', dest='radio', nargs='*', type=str, required=False)
-    parser.add_argument('-t', help="Download all TV videos; (Warning this takes some time and about 70GB) ", action='store', dest='tv', nargs='*', type=str, required=False)
-    parser.add_argument('-w', help="Download all default images from the wall;  ", action='store', dest='wall', nargs='*', type=str, required=False)
+    parser.add_argument('-v', help="Print verbose while running", action='store', dest='verbose',
+                        nargs='*', type=str, required=False)
+    parser.add_argument('-stationList', help="Print a list of stations", action='store', dest='stationList',
+                        nargs='*', type=str, required=False)
+    parser.add_argument('-musicList', help="Print a list of songs; Given a station name", action='store',
+                        dest='musicList', nargs='*', type=str, required=False)
+    parser.add_argument('-radio', help="Download all music & icons from a radio station; pass a file with station names to download more than one station", action='store', dest='radio', nargs='*', type=str, required=False)
+    parser.add_argument('-tv', help="Download all TV videos; (Warning this takes some time and about 70GB) ", action='store', dest='tv', nargs='*', type=str, required=False)
+    parser.add_argument('-graffiti', help="Download all default graffiti from the wall;  ", action='store', dest='graffiti', nargs='*', type=str, required=False)
     parser.add_argument('-wallpaper', help="Download wallpaper;  ", action='store', dest='wallpaper', nargs='*', type=str, required=False)
-
+    parser.add_argument('-all', help="Download Everything (make sure you have enough space);  ", action='store', dest='all', nargs='*',
+                        type=str, required=False)
     args = parser.parse_args()
+
+
+    if args.verbose is not None:
+        isVerbose[0] = True
+
+    # get the current list of stations
+    if args.stationList is not None:
+        print(getStations())
+
+    # Get the track list of stations
+    if args.musicList is not None:
+        for station in args.musicList:
+            print(getMusicList(station))
+
+    # Download a whole station
     if args.radio is not None:
         for station in args.radio:
             if os.path.isfile(station):
@@ -162,20 +230,31 @@ def main():
             else:
                 downloadRadio(station)
 
+    # Download the Tv
     if args.tv is not None:
-        with urllib.request.urlopen(tv_src) as response:
-            with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
-                shutil.copyfileobj(response, tmp_file)
-        downloadTV(open(tmp_file.name))
+        downloadTV()
 
-    if args.wall is not None:
+    # Download Graffiti
+    if args.graffiti is not None:
         downloadWall()
-    
+
+    # Download Wallpaper
     if args.wallpaper is not None:
         DownloadWallpaperImages()
+
+    # Download Everything
+    if args.all is not None:
+        stations = getStations()
+        for station in stations:
+            downloadRadio(station)
+        downloadWall()
+        DownloadWallpaperImages()
+        downloadTV()
+
+
+    exit(0)
 
 if __name__ == '__main__':
     main()
 
-exit()
 
